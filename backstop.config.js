ab2e00a91ff1de07b27148596dc7837564c7b877
Ashley Ward <421038+ashward@users.noreply.github.com>
2024-04-04 11:13:54 +0100

    Initial commit of visual regression testing

---
+++ backstop.config.js
@@ -0,0 +1,71 @@
+const baseUrl = 'https://www.teach-in-further-education.campaign.gov.uk';
+
+const pages = [
+  "/",
+  "/what-is-fe-teaching/",
+  "/whats-it-like-to-teach-in-fe/",
+  "/how-do-i-find-a-job-in-fe/",
+  "/find-local-colleges/",
+  "/find-local-colleges/?postcode=w1w+7rt",
+  "/sharing-is-caring/",
+  "/privacy-policy/",
+  "/cookies-policy/",
+  "/accessibility-statement/",
+]
+
+const scenarios = pages.map(page => {
+  return {
+    "label": `Page ${page}`,
+    "cookiePath": "backstop_data/engine_scripts/cookies.json",
+    "url": `${baseUrl}${page}`,
+    "referenceUrl": "",
+    "readyEvent": "",
+    "readySelector": "",
+    "delay": 1000,
+    "hideSelectors": [],
+    "removeSelectors": [],
+    "hoverSelector": "",
+    "clickSelector": "",
+    "postInteractionWait": 0,
+    "selectors": [],
+    "selectorExpansion": true,
+    "expect": 0,
+    "misMatchThreshold" : 0.2,
+    "requireSameDimensions": true
+  }
+})
+
+module.exports = {
+  id: "backstop_default",
+  viewports: [
+    {
+      "label": "phone",
+      "width": 320,
+      "height": 480
+    },
+    {
+      "label": "tablet",
+      "width": 1024,
+      "height": 768
+    }
+  ],
+  onBeforeScript: "puppet/onBefore.js",
+  onReadyScript: "puppet/onReady.js",
+  scenarios,
+  paths: {
+    "bitmaps_reference": "backstop_data/bitmaps_reference",
+    "bitmaps_test": "backstop_data/bitmaps_test",
+    "engine_scripts": "backstop_data/engine_scripts",
+    "html_report": "backstop_data/html_report",
+    "ci_report": "backstop_data/ci_report"
+  },
+  report: ["browser"],
+  engine: "puppet",
+  engineOptions: {
+    "args": ["--no-sandbox"]
+  },
+  asyncCaptureLimit: 5,
+  asyncCompareLimit: 50,
+  debug: false,
+  debugWindow: false
+}
