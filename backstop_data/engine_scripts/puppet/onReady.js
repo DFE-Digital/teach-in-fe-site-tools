cd5bca597159d3bf2fa51a49f92a28efd6f686a5
Ashley Ward <421038+ashward@users.noreply.github.com>
2024-04-04 15:34:59 +0100

    Trying forcing all images to be decoded

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -2,5 +2,14 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
 
+  // Force all images to be decoded
+  await page.evaluate(async () => {
+    var images = document.getElementsByTagName('img');
+
+    for(var i = 0; i < images.length ; i++) {
+      await images[i].decode();
+    }
+  })
+
   // add more ready handlers here...
 };
